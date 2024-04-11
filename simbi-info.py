8ba7e5460b63b3d8f7472f7edf10cb077bd040b2
Vinicius Roque <hownatios@gmail.com>
2024-04-11 12:14:56 -0300

    imp: remove revaluation logic

--- simbi-info.py
+++ simbi-info.py
@@ -12,7 +12,6 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix="!", intents=intents)
 
-# run(client.load_extension("logic.tasks.revaluation"))
 run(client.load_extension("logic.tasks.birth"))
 run(client.load_extension("logic.tasks.logs"))
 run(client.load_extension("logic.tasks.presentation"))
