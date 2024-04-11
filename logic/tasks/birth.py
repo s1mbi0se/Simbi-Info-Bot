0bd3338ea77f27603cb32e850b44cdaa42b480fa
Vinicius Roque <hownatios@gmail.com>
2024-04-11 12:12:01 -0300

    feat: refactor birth to use rocketry

--- logic/tasks/birth.py
+++ logic/tasks/birth.py
@@ -1,19 +1,21 @@
-import datetime
-import os
 from io import BytesIO
 
-import discord
 import httpx
 from config import Config
 from discord import File
-from discord.ext import commands, tasks
+from discord.ext import commands
+from rocketry import Rocketry
+from rocketry.conditions.api import daily
 
 from utils.birthday import get_birthdays
 from utils.get_time import get_time_from_api
 from utils.logging import write_error_log, write_report_log
 
-EVERY_DAY = datetime.time(
-    hour=Config.BIRTH_HOUR, minute=Config.BIRTH_MINUTE, tzinfo=Config.TIME_ZONE
+schedule = Rocketry(
+    config={
+        "task_execution": "async",
+        "timezone": Config.TIME_ZONE,
+    }
 )
 
 
@@ -26,140 +28,85 @@ class Birth(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
-
-        # Verifica se a mensagem foi enviada em um canal privado
-        if isinstance(message.channel, discord.DMChannel):
-            if message.content.startswith("hi"):
-                await message.channel.send("Hello!")
-
-            elif message.content.startswith("logs"):
-                log_files = ["error.log", "report.log"]
-
-                for log_file in log_files:
-                    if not os.path.exists(log_file):
-                        with open(log_file, "w") as file:
-                            file.write("\n")
-
-                    with open(log_file, "r") as file:
-                        lines = file.readlines()
-                        last_lines = lines[-10:]
-                        await message.channel.send(
-                            f"```markdown\n"
-                            f"# {log_file}\n\n"
-                            f"{''.join(last_lines)}```"
-                        )
-
-            else:
-                await message.channel.send(
-                    "```markdown\n"
-                    "# Aqui estão meus comandos: \n\n"
-                    "## hi\n"
-                    "> Com este comando, eu apenas retornarei Hello para você.\n"
-                    "## logs\n"
-                    "> Com este comando, eu retornarei "
-                    "os arquivos `error.log` e `report.log`."
-                    "```"
-                )
-
-    @commands.Cog.listener()
     async def on_ready(self):
         print("Birth is ready")
-        self.verify_sheet.start()
 
-    @tasks.loop(seconds=60)
-    async def verify_sheet(self):
-        now = get_time_from_api()
-        current_time = now.time()
+        @schedule.task(daily.at("08:00"), name="Verify birthdays")
+        async def verify_sheet():
+            now = get_time_from_api()
 
-        if (
-            current_time.hour != EVERY_DAY.hour
-            or current_time.minute != EVERY_DAY.minute
-        ):
-            return
+            print(f"SEARCHING BIRTH: {now.date()} - {now.time()}")
 
-        print(f"SEARCHING BIRTH: {now.date()} - {now.time()}")
+            write_report_log(sender="BIRTH", msg="Starting daily birth check")
 
-        write_report_log(sender="BIRTH", msg="Starting daily birth check")
+            try:
+                channel = self.bot.get_channel(Config.BIRTHDAY_CHANNEL)
 
-        try:
-            channel = self.bot.get_channel(Config.BIRTHDAY_CHANNEL)
+                LIST_OF_MEMBERS = get_birthdays()
 
-            LIST_OF_MEMBERS = get_birthdays()
+                discord_files = []
+                if LIST_OF_MEMBERS:
+                    names = ""
+                    pix = ""
 
-            discord_files = []
-            if LIST_OF_MEMBERS:
-                names = ""
-                pix = ""
+                    for pos, member in enumerate(LIST_OF_MEMBERS):
+                        names += f"{' e ' if pos > 0 else ''}**{member['Membro']}**"  # noqa
+                        pix += (
+                            f"Chave PIX para quem quiser/puder"
+                            f" enviar um presentinho para"
+                            f" {member['Membro'].split(' ')[0]}: {member['PIX']} \n"  # noqa
+                            if member["PIX"]
+                            else ""
+                        )
 
-                for pos, member in enumerate(LIST_OF_MEMBERS):
-                    names += f"{' e ' if pos > 0 else ''}**{member['Membro']}**"  # noqa
-                    pix += (
-                        f"Chave PIX para quem quiser/puder"
-                        f" enviar um presentinho para"
-                        f" {member['Membro'].split(' ')[0]}: {member['PIX']} \n"  # noqa
-                        if member["PIX"]
-                        else ""
+                        image_request = httpx.get(
+                            member.get("Foto"), follow_redirects=True
+                        )
+                        image_bytes = BytesIO(image_request.content)
+
+                        discord_file = File(
+                            image_bytes, filename=f"{member.get('Membro')}.png"
+                        )
+                        discord_files.append(discord_file)
+
+                    cargo_ids = Config.BIRTHDAY_CARGOS
+
+                    cargos = (
+                        [
+                            channel.guild.get_role(int(cargo_id))
+                            for cargo_id in cargo_ids
+                        ]
+                        if cargo_ids
+                        else None
                     )
 
-                    image_request = httpx.get(
-                        member.get("Foto"), follow_redirects=True
+                    cargo_mentions = (
+                        (
+                            " ".join(
+                                [cargo.mention for cargo in cargos if cargos]
+                            )
+                            + "\n"
+                        )
+                        if cargos
+                        else None
                     )
-                    image_bytes = BytesIO(image_request.content)
 
-                    discord_file = File(
-                        image_bytes, filename=f"{member.get('Membro')}.png"
+                    await channel.send(
+                        f"{cargo_mentions if cargo_mentions else ''}"
+                        f"**HOJE É DIA DE  COMEMORAR!**\n\n"
+                        f"Hoje comemoramos a vida de {names} 🎉 🎉\n"
+                        f"Parabéns! Muitas felicidades,"
+                        f" que este novo ciclo que se inicia "
+                        f"seja repleto de realizações e conquistas! \n\n"
+                        f"{pix}",
+                        files=discord_files,
                     )
-                    discord_files.append(discord_file)
+                else:
+                    return
+            except Exception as e:
+                write_error_log(sender="BIRTH", msg=str(e))
 
-                cargo_ids = Config.BIRTHDAY_CARGOS
-
-                cargos = (
-                    [
-                        channel.guild.get_role(int(cargo_id))
-                        for cargo_id in cargo_ids
-                    ]
-                    if cargo_ids
-                    else None
-                )
-
-                cargo_mentions = (
-                    (
-                        " ".join([cargo.mention for cargo in cargos if cargos])
-                        + "\n"
-                    )
-                    if cargos
-                    else None
-                )
-
-                # await channel.send(
-                #     f"{cargo_mentions if cargo_mentions else ''}"
-                #     f"**MINHAS SINCERAS E ELETRÔNICAS DESCULPAS...**\n\n"
-                #     f"Nesse mês tivemos o aniversário de {names}, mas eu estava fora do ar 💥💥\n"  # NOQA
-                #     f"De qualquer forma, vamos comemorar a vida de {names} 🎉 🎉\n"  # NOQA
-                #     f"Parabéns! Muitas felicidades,"
-                #     f" que este novo ciclo que se inicia "
-                #     f"seja repleto de realizações e conquistas! \n\n"
-                #     f"{pix}",
-                #     files=discord_files,
-                # )
-
-                await channel.send(
-                    f"{cargo_mentions if cargo_mentions else ''}"
-                    f"**HOJE É DIA DE  COMEMORAR!**\n\n"
-                    f"Hoje comemoramos a vida de {names} 🎉 🎉\n"
-                    f"Parabéns! Muitas felicidades,"
-                    f" que este novo ciclo que se inicia "
-                    f"seja repleto de realizações e conquistas! \n\n"
-                    f"{pix}",
-                    files=discord_files,
-                )
-            else:
-                return
-        except Exception as e:
-            write_error_log(sender="BIRTH", msg=str(e))
+        await schedule.serve()
 
 
 async def setup(bot):
