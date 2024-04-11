e71ab27fb57305572ae5415dbc3896b8f5b7d8ee
Vinicius Roque <hownatios@gmail.com>
2024-04-11 12:12:46 -0300

    fix: lint

--- logic/tasks/presentation.py
+++ logic/tasks/presentation.py
@@ -1,8 +1,11 @@
 import os
+
 from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
+
 from utils import generate_presentation
+
 load_dotenv()
 
 
@@ -16,18 +19,27 @@ class Presentation(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        allowed_channel_id = int(os.getenv("CHANNEL_ALLOWED_FOR_PRESENTATION", default=0))
-        if message.author == self.bot.user or message.channel.id != allowed_channel_id:
+        allowed_channel_id = int(
+            os.getenv("CHANNEL_ALLOWED_FOR_PRESENTATION", default=0)
+        )
+        if (
+            message.author == self.bot.user
+            or message.channel.id != allowed_channel_id
+        ):
             return
 
         if message.content.startswith("!mt-presentation"):
-            await message.channel.send("Um momento, estou preparando a apresentação...")
+            await message.channel.send(
+                "Um momento, estou preparando a apresentação..."
+            )
             current_sprint, presentation_url = generate_presentation.main()
             mt_role_name = os.getenv("MT_ROLE_NAME")
             role = get(message.guild.roles, name=mt_role_name)
             await message.channel.send(
-                f"{message.author.mention} e {role.mention } aqui está a apresentação da **Sprint Review {current_sprint}**: "
-                f"{presentation_url}")
+                f"{message.author.mention} e {role.mention } aqui está"
+                f" a apresentação da **Sprint Review {current_sprint}**: "
+                f"{presentation_url}"
+            )
 
     @commands.Cog.listener()
     async def on_ready(self):
