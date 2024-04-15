8a401c5eb32c05b7259eee246f5f53255548a684
Joao Kax <lukaxsilva.ls@gmail.com>
2024-04-15 16:47:39 -0300

    refactor: commands for mercado topografico

--- logic/tasks/mercado_topografico.py
+++ logic/tasks/mercado_topografico.py
@@ -1,3 +1,5 @@
+import os
+
 from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
@@ -28,30 +30,47 @@ class MercadoTopografico(commands.Cog):
         ):
             return
 
-        if message.content.startswith("!mt-presentation"):
-            await message.channel.send(
-                "Um momento, estou preparando a apresentação..."
-            )
-            current_sprint, presentation_url = generate_presentation()
-            mt_role_name = "squad 1"
-            # mt_role_name = os.getenv("MT_ROLE_NAME")
-            role = get(message.guild.roles, name=mt_role_name)
-            await message.channel.send(
-                f"{message.author.mention} e {role.mention } aqui está"
-                f" a apresentação da **Sprint Review {current_sprint}**: "
-                f"{presentation_url}"
-            )
+        if "!mt" in message.content:
+            if "presentation" in message.content:
+                await self.process_presentation_command(message)
+            elif "estimate" in message.content:
+                await self.process_estimate_command(message)
+            else:
+                await self.send_invalid_command_message(message)
 
-        if message.content.startswith("!mt-estimate"):
-            await message.channel.send(
-                "Um momento, estou pegando as estimativas..."
-            )
+    @staticmethod
+    async def process_presentation_command(message):
+        await message.channel.send(
+            "Um momento, estou preparando a apresentação..."
+        )
+        current_sprint, presentation_url = generate_presentation()
+        # mt_role_name = os.getenv("MT_ROLE_NAME")
+        mt_role_name = "squad 1"
+        role = get(message.guild.roles, name=mt_role_name)
+        await message.channel.send(
+            f"{message.author.mention} e {role.mention} aqui está"
+            f" a apresentação da **Sprint Review {current_sprint}**: "
+            f"{presentation_url}"
+        )
 
-            estimate_message = estimated_efforts()
+    @staticmethod
+    async def process_estimate_command(message):
+        await message.channel.send(
+            "Um momento, estou pegando as estimativas..."
+        )
+        estimate_message = estimated_efforts()
+        await message.channel.send(
+            f"{message.author.mention} {estimate_message}"
+        )
 
-            await message.channel.send(
-                f"{message.author.mention} {estimate_message}"
-            )
+    @staticmethod
+    async def send_invalid_command_message(message):
+        await message.channel.send(
+            f"### Comando inválido\n"
+            f"Use um dos comandos abaixo:\n"
+            f"**!mt-presentation** para gerar a apresentação da sprint review\n"
+            f"**!mt-estimate** para obter as estimativas da sprint atual"
+        )
 
     @commands.Cog.listener()
     async def on_ready(self):
