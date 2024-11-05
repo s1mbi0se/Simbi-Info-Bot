8598b027db09633540e564d1f6e6bb8089ae12ed
Vinicius Roque <hownatios@gmail.com>
2024-11-05 09:10:44 -0300

    fix: datetime api

--- utils/get_time.py
+++ utils/get_time.py
@@ -5,13 +5,12 @@ from config import Config
 
 
 def get_time_from_api():
-    response = httpx.get(
-        r"https://worldtimeapi.org/api/timezone/America/Sao_Paulo"
-    )
+    response = httpx.get("https://timeapi.io/api/time/current/zone?timeZone=America%2FSao_Paulo")
     data = response.json()
-    datetime_string = data["datetime"]
+    datetime_string = data["dateTime"][:26]
     datetime_object = datetime.datetime.strptime(
-        datetime_string, "%Y-%m-%dT%H:%M:%S.%f%z"
+        datetime_string, "%Y-%m-%dT%H:%M:%S.%f"
     )
     now = datetime_object.astimezone(Config.TIME_ZONE)
+    print("NOW: ", now)
     return now
